4cbd3070e1963d700ce7a5ac2fa44bbfa4b6056e
SATYAM KUMAR SINGH <101524549+Satyam9430308305@users.noreply.github.com>
2023-07-16 10:06:29 +0530

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,9 @@
+   echo "Enter the principal:"
+   read p
+   echo "Enter rate of interest per year:"
+   read r
+   echo "Enter time period in years:"
+   read t
+   s=`expr $p \* $t \* $r / 100`
+   echo "The simple interest is: "
+   echo $s
