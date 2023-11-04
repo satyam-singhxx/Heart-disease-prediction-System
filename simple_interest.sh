13a8110b4aaa02e61d593c1e752643c9fab8ec74
SATYAM KUMAR SINGH <101524549+Satyam9430308305@users.noreply.github.com>
2023-11-04 18:14:15 +0530

    Delete simple_interest.sh
    not part of this project

--- simple_interest.sh
+++
@@ -1,9 +0,0 @@
-   echo "Enter the principal:"
-   read p
-   echo "Enter rate of interest per year:"
-   read r
-   echo "Enter time period in years:"
-   read t
-   s=`expr $p \* $t \* $r / 100`
-   echo "The simple interest is: "
-   echo $s
